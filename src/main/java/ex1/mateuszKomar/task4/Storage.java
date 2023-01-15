b7064b18dcc2307727debf9208132d1aba85c948
Mateusz Komar <mateusz.komar1988@gmail.com>
2023-01-15 15:10:16 +0100

    Task 4 Resolved

---
+++ src/main/java/ex1/mateuszKomar/task4/Storage.java
@@ -0,0 +1,40 @@
+package ex1.mateuszKomar.task4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Storage {
+    private Map<String, List<String>> storageMap = new HashMap<>();
+
+
+    public void addToStorage(String key, String name) {
+
+        List<String> valueList = storageMap.get(key);
+        if (valueList == null) {
+            valueList = new ArrayList<>();
+        }
+
+        valueList.add(name);
+        storageMap.put(key, valueList);
+        storageMap.forEach((k, v) -> System.out.println(k + " " + v));
+    }
+
+    public void printValues(String key) {
+
+        if (storageMap.get(key) == null) {
+            System.out.println("Tis key doesn't ex");
+        } else {
+            System.out.println(storageMap.get(key));
+        }
+    }
+
+    public void findKeys(String value) {
+        storageMap.forEach((k, valueList) -> {
+            if (valueList.contains(value)) {
+                System.out.println(k);
+            }
+        });
+    }
+}
---
+++ src/main/java/ex1/mateuszKomar/task4/Task4Main.java
@@ -0,0 +1,29 @@
+package ex1.mateuszKomar.task4;
+
+public class Task4Main {
+    public static void main(String[] args) {
+        Storage storage = new Storage();
+        storage.addToStorage("animals", "horse");
+        storage.addToStorage("animals", "dog");
+        storage.addToStorage("animals", "cat");
+
+
+        storage.addToStorage("cars", "Merc");
+        storage.addToStorage("cars", "BMW");
+        storage.addToStorage("cars", "Audi");
+        storage.addToStorage("cars", "dog");
+
+
+
+        storage.printValues("Animal");
+        storage.printValues("cars");
+        storage.printValues("books");
+
+        System.out.println("**************************************");
+
+        storage.findKeys("dog");
+
+
+
+    }
+}
