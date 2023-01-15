f7af9924ca89a503a2d9a8dbd39baaa7739adfc5
Mateusz Komar <mateusz.komar1988@gmail.com>
2023-01-15 13:58:14 +0100

    Task 3 Resolved

--- src/main/java/ex1/mateuszKomar/task3/MapPrinter.java
+++ src/main/java/ex1/mateuszKomar/task3/MapPrinter.java
@@ -5,7 +5,22 @@ import java.util.Map;
 public class MapPrinter {
     public void print(Map<String, Integer>mapToPrint){
         mapToPrint.forEach((k,v)->{
-            System.out.println("Klucz: " + k + " Wartość: " + v);
+            System.out.println("Klucz: "  + k + ","  + " Wartość: " + v + "," );
         });
+
+        final var iterator = mapToPrint.entrySet().iterator();
+
+        System.out.println("\nINTERATOR LOOP");
+        while (iterator.hasNext()){ //dopóki jest następny to:
+            final var nextEntry = iterator.next(); //pobierz nastepny
+            if (iterator.hasNext()){
+                System.out.println("Klucz: "  + nextEntry.getKey() + "," + " Wartość: " + nextEntry.getValue() + "," );
+            } else {
+                System.out.println("Klucz: "    + nextEntry.getKey() + "," + " Wartość: " + nextEntry.getValue() + "." );
+            }
+            //sprawdź czy jest nastepny, jesli tak to wstaw przecinek, jesli nie to kropka
+
+            //System.out.println("Klucz: " +  ", " + nextEntry.getKey() + " Wartość: " + nextEntry.getValue() + "." ); //drukuj z kropką lub przecinkiem
+        }
     }
 }
