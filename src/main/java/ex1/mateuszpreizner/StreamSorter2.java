12c4ea5c0e7252237ef6e33b66136d4275469eb7
Mateusz Preizner <preizner.mateusz@gmail.com>
2023-01-15 12:25:29 +0100

    resolved exerciseTwo

---
+++ src/main/java/ex1/mateuszpreizner/StreamSorter2.java
@@ -0,0 +1,14 @@
+package ex1.mateuszpreizner;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+    public class StreamSorter2 {
+        public List<String> sortDesc2(List<String> unsortedList2) {
+            return unsortedList2.stream()
+                    .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                    .collect(Collectors.toList());
+
+        }
+    }
