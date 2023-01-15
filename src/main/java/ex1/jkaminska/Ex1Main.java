6c568f1687024e650243a69e7ef0837d5d457c96
julia-kaminska <kaminska.julia@interia.pl>
2023-01-15 11:53:16 +0100

    jkaminska-ex1 solved

--- src/main/java/ex1/jkaminska/Ex1Main.java
+++ src/main/java/ex1/jkaminska/Ex1Main.java
@@ -1,4 +1,34 @@
 package ex1.jkaminska;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ex1Main {
+    public static void main(String[] args) {
+
+        final var unsortedStrings = new ArrayList<String>();
+      //  List<String> list = new ArrayList<>(); --> same as the one above
+        unsortedStrings.add("Tomasz");
+        unsortedStrings.add("Bartosz");
+        unsortedStrings.add("Małgorzata");
+        System.out.println("INPUT: " + unsortedStrings);
+
+//        final var unsortedStrings2 = List.of(    //utowrzenie takiej listy nie pozwala jeej zmieniać
+//                "Tomasz",
+//                "Bartosz",
+//                "Małgorzata"
+//        );
+
+        final var expected = new ArrayList<String>();
+        expected.add("Tomasz");
+        expected.add("Małgorzata");
+        expected.add("Bartosz");
+        System.out.println("EXPECTED: " + expected);
+
+        final var sorter = new StreamSorter();
+        final var result = sorter.sortDesc(unsortedStrings);
+        System.out.println("RESULT: " + result);
+
+    }
+
 }
---
+++ src/main/java/ex1/jkaminska/ListSorter.java
@@ -0,0 +1,12 @@
+package ex1.jkaminska;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ListSorter {
+    public List <String> sortDesc(List<String> unsortedList){
+        unsortedList.sort(Comparator.reverseOrder());
+        return unsortedList;
+    }
+}
