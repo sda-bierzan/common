f87d9d292ed17682170020015307c8f00d246751
Pjoter K <piotrz9522@gmail.com>
2023-01-15 11:53:52 +0100

    pkimszal

---
+++ src/main/java/ex1/pkimszal/ListSorter.java
@@ -0,0 +1,14 @@
+package ex1.pkimszal;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ListSorter {
+
+    public List<String> sortDesc(List<String> unsortedList) {
+        unsortedList.sort(Comparator.reverseOrder());
+
+
+        return unsortedList;
+    }
+}
--- src/main/java/ex1/pkimszal/ex1main.java
+++ src/main/java/ex1/pkimszal/ex1main.java
@@ -1,7 +1,28 @@
 package ex1.pkimszal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ex1main {
     public static void main(String[] args) {
 
+
+        final var unsortedStrings = new ArrayList<String>();
+        unsortedStrings.add("Tomasz");
+        unsortedStrings.add("Pjoter");
+        unsortedStrings.add("Bartunia");
+        System.out.println("INPUT: " + unsortedStrings);
+
+        final var expected = new ArrayList<String>();
+        expected.add("Tomasz");
+        expected.add("Bartunia");
+        expected.add("Pjoter");
+        System.out.println("EXPECTED: " + expected);
+
+        final var sorter = new LoopSorter();
+        final var result = sorter.sortDesc(unsortedStrings);
+        System.out.println("RESULT: " + result);
+
     }
+
 }
