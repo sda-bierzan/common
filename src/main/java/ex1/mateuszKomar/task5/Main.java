c958c622fbd6bf17357ea011277b9012b224cafc
Mateusz Komar <mateusz.komar1988@gmail.com>
2023-01-17 18:54:39 +0100

    Task 5 Resolved part 2

--- src/main/java/ex1/mateuszKomar/task5/Main.java
+++ src/main/java/ex1/mateuszKomar/task5/Main.java
@@ -3,6 +3,11 @@ package ex1.mateuszKomar.task5;
 public class Main {
     public static void main(String[] args) {
 
+        SDAHashSet sdaHashSet = new SDAHashSet();
+        sdaHashSet.add("Mateusz");
+        sdaHashSet.add("Bartek");
+        System.out.println(sdaHashSet);
+
 
     }
 
